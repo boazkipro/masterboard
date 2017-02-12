a7e5303a32fe70a829e9dc50e75c87ac722f85ae
Boaz <boazkipro93@gmail.com>
2017-02-13 01:26:00 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,64 @@
+/* global app */
+/* global app_hlink */
+/* global app_ip */
+/* global app_port */
+
+global.app = require('./assets/js/app.js');
+
+//! CUSTOM EXTENTIONS HERE
+
+    //* EXTEND Object to cater for {{Object}}.keys
+    function keys(){
+        var k = [];
+        for(var p in this) {
+            if(this.hasOwnProperty(p))
+                k.push(p);
+        }
+        return k;
+    }
+    Object.defineProperty(Object.prototype, "keys", { value : keys, enumerable:false });
+
+    //* CLONE A JAVASCRIPT OBJECT
+    global.clone = function( obj ){
+        return JSON.parse(JSON.stringify(obj));
+    };    
+
+    //* EXTEND THE Array TO CATER FOR {{Array}}.inArray
+    Array.prototype.has = function( needle ){
+
+        return Array(this).join(",").indexOf(needle) >-1;
+
+    };
+
+   //* COUNT INSTANCES IN ARRAY
+    Array.prototype.count = function( val ){
+       
+	  if( val === undefined ){
+		  return this.length;
+	  }else{
+		 
+		 var counter = 0;
+		 
+		 this.forEach(function(ElementValue,ElementPosition){
+		  
+		  	if( val == ElementValue ){
+				counter++;	  
+			}
+		  
+	  	 });
+		   
+		return counter;
+		  
+	  }
+      
+        
+    };
+
+//! EO - CUSTOM EXTENSIONS
+
+
+//! APP IMPORTS
+require("./assets/js/app-router.js");
+require('./assets/js/services/services.js');
+require('./assets/js/directives/directives.js');
+require('./assets/js/controllers/controllers.js');
